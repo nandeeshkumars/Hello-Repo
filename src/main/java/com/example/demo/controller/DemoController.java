4e9d9d646239f915fe5784030c20f815027418d5
nandeeshkumars <110682885+nandeeshkumars@users.noreply.github.com>
2022-08-10 18:22:15 +0530

    First commit for project

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to Hello World";
+	}
+}
